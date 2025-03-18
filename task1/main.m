d92e05643d37ba9811904dbc88f1b7cb21e8fa30
javadcc_mac <javadcc1@sjtu.edu.cn>
2025-03-18 10:27:16 +0800

    spline->interp1

--- task1/main.m
+++ task1/main.m
@@ -17,10 +17,10 @@ voltage_test = test_data_matrix(2:2:end, :);
 train_data_size = size(voltage_train, 1);
 
 % 参数设置
-population_size = 200;
+population_size = 1000;
 epochs = 100;
-crossover_rate = 0.8;
-variation_rate = 0.2;
+crossover_rate = 0.6;
+variation_rate = 0.4;
 elite_num = 5;   % 考虑保留一部分最优个体
 record_cost = zeros(epochs, 1);
 
@@ -28,7 +28,7 @@ record_cost = zeros(epochs, 1);
 % 布尔数组标记温度采样点，标准任务指定了三次样条插值，据此获得曲线计算适应度
 
 % 生成初始种群
-probability = 0.2; % 指定每个基因为1的概率
+probability = 0.1; % 指定每个基因为1的概率
 population = rand(population_size, length(temperture)) < probability; % [population_size, 90]
 fitness = zeros(population_size, 1);
 
@@ -39,10 +39,10 @@ for i = 1:epochs
     interpolation = zeros(population_size, train_data_size, length(temperture));
     for j = 1:population_size
         x = temperture(population(j,:) == 1); % 当前解对应的温度采样点
-        for k = 1:train_data_size
+        parfor k = 1:train_data_size
             y_k = voltage_train(k, population(j,:) == 1); % 第k组数组的测定电压值 据此插值出90个点
             % 插值 方式是通过第k组电压值索引采样出对应的90个温度值 后续只需和标准的-20～69比对即可
-            interpolation(j, k, :) = spline(y_k, x, voltage_train(k, :));
+            interpolation(j, k, :) = interp1(y_k, x, voltage_train(k, :), 'spline');
         %     plot(squeeze(interpolation(j, k, :)), voltage_train(k, :), 'r');
         %     hold on;
         %     plot(temperture, voltage_train(k, :), 'b');
@@ -75,3 +75,5 @@ for i = 1:epochs
     disp(['当前最优解：', num2str(find(elite_population(1,:) == 1))]);
 
 end
+
+toc;
